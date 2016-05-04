5737507c5738801a1e209b7adfbb19500dd0b915
niusmallnan <zhangzb@neunn.com>
2016-05-04 08:58:31 +0000

    format indent

--- canpian/admin.py
+++ canpian/admin.py
@@ -6,7 +6,7 @@ from .models import Canpian, Dynasty, Image
 class ImageInlineAdmin(admin.TabularInline):
     model = Image
     readonly_fields = ('render_image',)
-    
+
     def render_image(self, obj):
         return mark_safe("""<img src="%s?imageView2/2/w/600" />""" % obj.file.url)
 
@@ -14,9 +14,9 @@ class ImageInlineAdmin(admin.TabularInline):
 class CanpianAdmin(admin.ModelAdmin):
     list_filter = ('dynasty__name', 'completeness')
     search_fields = ('name',)
-    list_display = ('name', 'qly_id', 'dynasty', 'weight', 
+    list_display = ('name', 'qly_id', 'dynasty', 'weight',
                     'diameter', 'thickness', 'completeness')
-    
+
     inlines = [ImageInlineAdmin,]
 
 
--- canpian/models.py
+++ canpian/models.py
@@ -9,21 +9,23 @@ class Canpian(models.Model):
     COMPLETE = 'complete'
     INCOMPLETE = 'incomplete'
     COMPLETE_CHOICES = (
-	(COMPLETE, '完整'),
-	(INCOMPLETE, '残缺'),
+	    (COMPLETE, '完整'),
+	    (INCOMPLETE, '残缺'),
     )
 
     qly_id = models.PositiveIntegerField(verbose_name='本地编号')
     name = models.CharField(max_length=60, verbose_name='名称')
-    dynasty = models.ForeignKey('Dynasty', on_delete=models.CASCADE, verbose_name='年代')
+    dynasty = models.ForeignKey('Dynasty', on_delete=models.CASCADE,
+                                verbose_name='年代')
     weight = models.FloatField(verbose_name='重量', help_text='单位克')
     diameter = models.FloatField(verbose_name='直径', help_text='单位厘米')
     thickness = models.FloatField(verbose_name='厚度', help_text='单位毫米')
     remark = models.TextField(verbose_name='备注')
-    completeness = models.CharField(max_length=10, choices=COMPLETE_CHOICES, 
+    completeness = models.CharField(max_length=10, choices=COMPLETE_CHOICES,
 				    default=INCOMPLETE, verbose_name='是否完整')
 
-    created_at = models.DateTimeField(auto_now_add=True, verbose_name='创建时间')
+    created_at = models.DateTimeField(auto_now_add=True,
+                                      verbose_name='创建时间')
     updated_at = models.DateTimeField(auto_now=True, verbose_name='更新时间')
     #deleted_at = models.DateTimeField()
 
@@ -31,7 +33,7 @@ class Canpian(models.Model):
         return self.name
 
     class Meta:
-	verbose_name = '铜镜残片'
+        verbose_name = '铜镜残片'
         verbose_name_plural = '铜镜残片'
 
 
@@ -39,18 +41,19 @@ class Dynasty(models.Model):
     name = models.CharField(max_length=30, verbose_name='名称')
     start_year = models.SmallIntegerField(verbose_name='开始年份')
     end_year = models.SmallIntegerField(verbose_name='终止年份')
-    
+
     def __unicode__(self):
         return self.name
 
     class Meta:
-	verbose_name = '年代'
+        verbose_name = '年代'
         verbose_name_plural = '年代'
 
 
 class Image(models.Model):
     file = models.FileField(upload_to='canpian/')
-    canpian = models.ForeignKey('Canpian', related_name='images', blank=True, null=True)
+    canpian = models.ForeignKey('Canpian', related_name='images',
+                                blank=True, null=True)
 
     def __unicode__(self):
         return self.filename
@@ -60,7 +63,7 @@ class Image(models.Model):
         return self.file.name.rsplit('/', 1)[-1]
 
     class Meta:
-	verbose_name = '图片'
+        verbose_name = '图片'
         verbose_name_plural = '图片'
 
 
