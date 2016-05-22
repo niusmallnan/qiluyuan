08016a209c6786e08124852b726bec96f20d166a
niusmallnan <zhangzb@neunn.com>
2016-05-22 12:48:01 +0000

    add yachang data

--- yachang/models.py
+++ yachang/models.py
@@ -6,14 +6,24 @@ from django.db import models
 # Create your models here.
 
 class BronzeMirror(models.Model):
-    name = models.CharField(max_length=100)
-    size = models.CharField(max_length=50)
-    desc = models.TextField()
-    create_age = models.CharField(max_length=50)
-    valuation = models.CharField(max_length=50)
-    deal = models.CharField(max_length=100)
-    auction_company = models.CharField(max_length=100)
-    auction_meet = models.CharField(max_length=100)
-    auction_session = models.CharField(max_length=100)
-    url = models.URLField()
-    file_urls = models.CharField(max_length=200)
+    yc_id = models.CharField(max_length=50, verbose_name='雅昌编号')
+    name = models.CharField(max_length=100, verbose_name='名称')
+    size = models.CharField(max_length=50, verbose_name='尺寸')
+    desc = models.TextField(verbose_name='信息简介')
+    create_age = models.CharField(max_length=50, verbose_name='年代')
+    valuation = models.CharField(max_length=50, verbose_name='估价')
+    deal = models.CharField(max_length=100, verbose_name='成交价')
+    auction_company = models.CharField(max_length=100, verbose_name='拍卖公司')
+    auction_meet = models.CharField(max_length=100, verbose_name='拍卖会')
+    auction_session = models.CharField(max_length=100, verbose_name='拍卖专场')
+    auction_date = models.DateField(verbose_name='拍卖时间')
+    url = models.URLField(verbose_name='雅昌网链接')
+    file_urls = models.CharField(max_length=200, verbose_name='图片')
+
+    class Meta:
+        verbose_name = '铜镜拍卖'
+        verbose_name_plural = '铜镜拍卖'
+        ordering = ['-auction_date']
+
+    def __unicode__(self):
+        return self.name
