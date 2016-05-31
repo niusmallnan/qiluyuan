7b3dc926e866d362eff4d3f4f19f8f693482fc2f
niusmallnan <zhangzb@neunn.com>
2016-05-31 07:17:54 +0000

    index redirect admin

--- qiluyuan/urls.py
+++ qiluyuan/urls.py
@@ -18,11 +18,14 @@ from django.conf.urls import url, include, patterns
 from django.contrib import admin
 from django.conf.urls.static import static
 from django.conf import settings
+from django.views.generic.base import RedirectView
+from django.core.urlresolvers import reverse_lazy
 
 admin.site.site_header = '栖甪苑数据管理平台'
 admin.site.site_title = '栖甪苑'
 admin.site.index_title = '数据管理'
 
 urlpatterns = [
+    url(r'^$', RedirectView.as_view(url=reverse_lazy('admin:index'))),
     url(r'^admin/', admin.site.urls),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
